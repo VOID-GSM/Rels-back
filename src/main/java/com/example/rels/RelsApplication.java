c07c2639192348f48fa8fe5acba8d02d3cda726f
user <sooooooobinn@gmail.com>
2026-04-08 10:13:39 +0900

    feat: init

---
+++ src/main/java/com/example/rels/RelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rels/RelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
