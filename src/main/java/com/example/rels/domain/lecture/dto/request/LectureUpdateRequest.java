0de44cd87347533ec46df5b4bca2b366f8f83cc8
user <sooooooobinn@gmail.com>
2026-08-27 20:38:33 +0900

    refactor: 신청 마감일 필드 제거

--- src/main/java/com/example/rels/domain/lecture/dto/request/LectureUpdateRequest.java
+++ src/main/java/com/example/rels/domain/lecture/dto/request/LectureUpdateRequest.java
@@ -1,17 +1,13 @@
 package com.example.rels.domain.lecture.dto.request;
 
-
-
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Map;
-
 
 public record LectureUpdateRequest(
 	@NotBlank @Size(max = 100) String title,
@@ -20,7 +16,6 @@ public record LectureUpdateRequest(
 	Integer totalCapacity,
 	@NotBlank @Size(max = 255) String lectureLocation,
 	@NotNull LocalDate lectureDate,
-	@NotNull LocalTime lectureTime,
-	@NotNull LocalDateTime applicationDeadline
+	@NotNull LocalTime lectureTime
 ) {
 }
